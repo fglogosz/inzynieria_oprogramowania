e0e82821be3700454a102a2cd9e17b8ac63170ed
Filip <Filip@DESKTOP-GMBA5QN>
2022-01-22 17:51:19 +0100

    Add project files.

---
+++ Projekt_Io/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projekt_Io.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
