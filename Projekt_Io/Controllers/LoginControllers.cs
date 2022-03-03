1781f53e71375e4f7129e2b9293354fe9bef7e5b
Filip <Filip@DESKTOP-GMBA5QN>
2022-03-03 11:19:31 +0100

    Login

---
+++ Projekt_Io/Controllers/LoginControllers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projekt_Io.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_Io.Controllers
+{
+    public class LoginControllers : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if (userModel.UserName == "BillGates" && userModel.Password == "bigbucks")
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else
+            {
+                return View("LoginFailure", userModel);
+            }
+        }
+    }
+}
